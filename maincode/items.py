227270e8569a4f7ac7d155144b7cf47f083c6a2f
JubEfrei <julien.casamian@efrei.net>
2024-03-22 13:16:52 +0100

    update

--- maincode/items.py
+++ maincode/items.py
@@ -8,3 +8,19 @@ class Item(pygame.sprite.Sprite):
         self.pos = pos
         self.image = pygame.transform.rotate(pygame.transform.scale_by(pygame.image.load("square.png"), 0.1), 90)
         self.rect = self.image.get_rect(center=self.pos)
+
+class Shield(Item):
+    def __init__(self, pos):
+        Item.__init__(self, pos)
+
+    def give_pu(self, ship):
+        ship.shield += 1
+
+
+class Heal(Item):
+    def __init__(self, pos):
+        Item.__init__(self, pos)
+
+    def give_pu(self, ship):
+        ship.hp = 3
+
